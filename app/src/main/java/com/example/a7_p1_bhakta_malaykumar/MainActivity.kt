d7edcc2490be340db1003319655be89431f024b4
Malay Bhakta <malaybhakta10@gmail.com>
2019-11-21 15:03:11 -0600

    Mostly Functional

--- app/src/main/java/com/example/a7_p1_bhakta_malaykumar/MainActivity.kt
+++ app/src/main/java/com/example/a7_p1_bhakta_malaykumar/MainActivity.kt
@@ -1,18 +1,23 @@
 package com.example.a7_p1_bhakta_malaykumar
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
+import net.objecthunter.exp4j.ExpressionBuilder
 import java.lang.Exception
 
 
 class MainActivity : AppCompatActivity() {
 
+    var decimal : Boolean = true
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        tvZero.setOnClickListener { appendOnExpression("0", canClear = true) }
         tvOne.setOnClickListener { appendOnExpression("1",true) }
         tvTwo.setOnClickListener { appendOnExpression("2",true) }
         tvThree.setOnClickListener { appendOnExpression("3",true) }
@@ -22,7 +27,7 @@ class MainActivity : AppCompatActivity() {
         tvSeven.setOnClickListener { appendOnExpression("7",true) }
         tvEight.setOnClickListener { appendOnExpression("8",true) }
         tvNine.setOnClickListener { appendOnExpression("9",true) }
-
+        tvDecimal.setOnClickListener { appendOnExpression(".", true)  }
 
         tvPlus.setOnClickListener { appendOnExpression("+",false) }
         tvMinus.setOnClickListener { appendOnExpression("-",false) }
@@ -32,29 +37,51 @@ class MainActivity : AppCompatActivity() {
         tvParen.setOnClickListener { appendOnExpression("()",false) }
         tvClealear.setOnClickListener {
             tvExpression.text= ""
-            tvResult.text = ""}
+            tvResult.text = ""
+            tvExpression.setBackgroundColor(Color.WHITE)
+            tvExpression.setTextColor(Color.GRAY)
+        }
         //tvChangeSign.setOnClickListener { appendOnExpression(1,true) }
 
         tvResult.setOnClickListener { try {
+            //val expression = (1+2).build()
             val expression = ExpressionBuilder(tvExpression.text.toString()).build()
             val result = expression.evaluate()
+            if(result < 0){
+                tvExpression.setBackgroundColor(Color.BLACK)
+                tvExpression.setTextColor(Color.WHITE)
+            }
             val longResult = result.toLong()
-            if (result == longResult.toDouble())
-                tvResult.text = longResult.toString()
+            if (result == longResult.toDouble()) {
+                tvExpression.text = longResult.toString()
+            }
             else
-                tvResult.text = result.toString(0)
+                tvExpression.text = result.toString()
 
         }catch (e: Exception){
             Log.d("Exception","message: " + e.message)
+            tvExpression.text = "0"
+            //tvResult.text = "0"
+            tvExpression.setBackgroundColor(Color.RED)
         }}
-
-
     }
 
     fun appendOnExpression(string: String, canClear: Boolean){
+
+        tvExpression.setTextColor(Color.GRAY)
+        tvExpression.setBackgroundColor(Color.WHITE)
+
+        if(decimal && string == "."){
+            tvExpression.append(string)
+            Log.d("message", "decimal: " +decimal)
+            decimal = false
+            return
+        }
         if ( canClear){
             tvResult.text = ""
             tvExpression.append(string)
+            tvExpression.setBackgroundColor(Color.WHITE)
+            tvExpression.setTextColor(Color.GRAY)
         }
         else{
             tvExpression.append(tvResult.text)
